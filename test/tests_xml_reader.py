64b620c161d8254ee2f25599265022ae2dd9c765
Marco <Marco.Gotthans@googlemail.com>
2020-08-18 08:37:13 +0200

    added xml setup + teardown of test files
    committed early to test CI

--- test/tests_xml_reader.py
+++ test/tests_xml_reader.py
@@ -1,11 +1,38 @@
+import os
+import shutil
+
 import pytest
 
+from src.converter.bpmn_models.bpmn_enum import BPMNEnum
 from src.converter.xml_reader import XMLReader
 
 
 class TestXMLReader:
 
-    pytest.fixture(autouse=True)
+    @classmethod
+    def setup_class(self):
+        # copies test files into new folder
+        src = r'..\test\test_files\xml'
+        dst = r'..\test\test_files\working_dict'
+        src_abs = os.path.abspath(src)
+        dst_abs = os.path.abspath(dst)
+
+        try:
+            shutil.copytree(src_abs, dst_abs)
+        except (FileExistsError):
+            pass
+        except (Exception):
+            raise
+
+    @classmethod
+    def teardown_class(self):
+        # deletes folder with test files after tests
+        dst = r'..\test\test_files\working_dict'
+        dst_abs = os.path.abspath(dst)
+        shutil.rmtree(dst_abs)
+        pass
+
+    @pytest.fixture(autouse=True)
     def xml_reader(self):
         self.xml_reader = XMLReader()
 
