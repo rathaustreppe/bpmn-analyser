1b3a23eb2eb5a365f092f056fb9babf9d1570986
Marco <Marco.Gotthans@googlemail.com>
2020-11-06 13:16:50 +0100

    added syntax similiarity check of strings

--- src/nlp/synonym_composite.py
+++ src/nlp/synonym_composite.py
@@ -35,9 +35,12 @@ class SynonymComposite:
         if self.synset is not None:
             synset = self.tagged_word_to_synset(tagged_word=tagged_word)
             return self._synset_comparison(synsets_to_check=synset)
-        # check synonyms via simple text compare
+        # check synonyms via simple text compare and simple distance algorithm
+        # in case there are spelling mistakes
         if self.word is not None:
-            return tagged_word[0] == self.word
+            accepted_distance = 0.92
+            score = self.cosine_distance(word1=tagged_word[0], word2=self.word)
+            return score > accepted_distance
 
     def tagged_word_to_synset(self,
                               tagged_word: Tuple[str, str]) -> List[Synset]:
@@ -82,6 +85,36 @@ class SynonymComposite:
             return wordnet.ADV
         else:
             return ''
+
+    def cosine_distance(self, word1: str, word2: str):
+        # algorithm for calculating distance of two strings
+        # Example: Word1 == Word2 == 'hello' --> score: 1.0, means exact
+        # Example: Word1 == 'abc', Word2 == 'xyz' --> score: 0.0
+        # Example: Word1 == 'ready', Word2 == 'reedy' --> score: 0.9
+        set1 = {w for w in word1}
+        set2 = {w for w in word2}
+
+        l1 = []
+        l2 = []
+
+        # form a set containing keywords of both strings
+        rvector = set1.union(set2)
+        for w in rvector:
+            if w in set1:
+                l1.append(1)  # create a vector
+            else:
+                l1.append(0)
+            if w in set2:
+                l2.append(1)
+            else:
+                l2.append(0)
+        c = 0
+
+        # cosine formula
+        for i in range(len(rvector)):
+            c += l1[i] * l2[i]
+        cosine = c / float((sum(l1) * sum(l2)) ** 0.5)
+        return cosine
 
     def __str__(self) -> str:
         if self.synset is not None:
