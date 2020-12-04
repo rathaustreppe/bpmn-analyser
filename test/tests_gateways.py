2a66f44da64979dc7d5b13ba67efd5eacaeb62aa
Marco <Marco.Gotthans@googlemail.com>
2020-12-04 12:52:47 +0100

    cleanup deprecated examples

--- test/tests_gateways.py
+++ test/tests_gateways.py
@@ -4,9 +4,7 @@ from typing import List
 import pytest
 
 from src.converter.converter import Converter
-from src.examples.gateway_example import GatewayExample
-from src.exception.gateway_errors import ExclusiveGatewayBranchError, \
-    BranchingGatewayError
+from src.exception.gateway_errors import BranchingGatewayError
 from src.graph_pointer import GraphPointer
 from src.models.running_token import RunningToken
 from src.models.token import Token
@@ -90,14 +88,50 @@ class TestGateway:
         assert return_token == solution_token
 
     def test_interlaced_gateways_2(self, xml_folders_path):
-        gateway_example = GatewayExample()
+        init_token = RunningToken(attributes={
+            'v1': '1',
+            'v2': '1',
+            'def3': False,
+            'jkl1': False,
+            'jkl2': False,
+            'mno1': False,
+            'mno2': False,
+            'pqr': False
+            }
+        )
+
+        solution_token = Token(attributes={
+            'v1': '1',
+            'v2': '1',
+            'def3': True,
+            'jkl1': True,
+            'jkl2': False,
+            'mno1': True,
+            'mno2': True,
+            'pqr': True
+            }
+        )
+
+        grammar = r"""
+                NN_Chunk:     {<NN.?>}
+                """
+        chunker = Chunker(chunk_grams=grammar)
+
+        all_attributes_to_look_for = ['def3', 'jkl1', 'jkl2',
+                                      'mno1', 'mno2', 'pqr']
+        ruleset = []
+        for attribute in all_attributes_to_look_for:
+            syncloud = SynonymCloud.from_list(text=[attribute])
+            mod = TokenStateModification(key=attribute, value=True)
+            ruleset.append(TokenStateRule(state_conditions=[],
+                                          state_modifications=[mod],
+                                          synonym_cloud=syncloud))
 
         return_token = self.execute_process(filename='gateway_example.bpmn',
                                             xml_folders_path=xml_folders_path,
-                                            chunker=gateway_example.get_chunker(),
-                                            ruleset=gateway_example.get_ruleset(),
-                                            init_token=gateway_example.get_init_token())
-        solution_token = gateway_example.get_solution_token()
+                                            chunker=chunker,
+                                            ruleset=ruleset,
+                                            init_token=init_token)
 
         assert solution_token == return_token
 
